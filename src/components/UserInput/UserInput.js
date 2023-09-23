562d470408c4c21f40dabad813d9e44359129d41
rastislav <rabvoc@gmail.com>
2023-09-23 17:01:03 +0200

    Handling Events and Managing State
    Add Interactive input values

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,4 +1,5 @@
 import classes from "./UserInput.module.css";
+import { useState } from "react";
 import {
   TextField,
   Button,
@@ -11,84 +12,122 @@ import {
 } from "@mui/material";
 
 const UserInput = () => {
+  const [userInput, setUserInput] = useState({
+    name: "John",
+    email: "john@mail.com",
+    field: "Marketing",
+  });
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("SUBMIT");
+    //...todo
+  };
+
+  const resetHandler = (event) => {
+    event.preventDefault();
+    setUserInput({
+      name: "",
+      email: "",
+      field: "",
+    });
+  };
+
+  const inputChangeHandler = (input, value) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input]: value,
+      };
+    });
+  };
 
   return (
     <div className={classes.form}>
       <h1>Add new visitor</h1>
-      <FormControl className={classes.form}> 
-        <TextField
-          id="name"
-          label="Full name"
-          type="text"
-          value={"John"}
-          sx={{
-            width: "400px",
-          }}
-        />
-        <TextField
-          id="email"
-          label="Email adress *"
-          type="email"
-          value={"john@gmail.com"}
-          sx={{
-            width: "400px",
-          }}
-        />
-        <TextField
-          id="select"
-          value={"Marketing"}
-          select
-          label="Department"
-          sx={{
-            width: "400px",
-          }}
-        >
-          <MenuItem key={1} value={"Accounting"}>
-            Accounting
-          </MenuItem>
-          <MenuItem key={2} value={"IT"}>
-            IT
-          </MenuItem>
-          <MenuItem key={3} value={"Management"}>
-            Management
-          </MenuItem>
-          <MenuItem key={4} value={"Marketing"}>
-            Marketing
-          </MenuItem>
-          <MenuItem key={5} value={"Sales"}>
-            Sales
-          </MenuItem>
-        </TextField>
-        <FormControlLabel
-          control={<Checkbox />}
-          label="I agree to be added to the table"
-        />
-        <Stack spacing={2} direction="row">
-          <Button
-            type="reset"
-            variant="outlined"
-            color="warning"
+      <form onSubmit={submitHandler}>
+        <FormControl className={classes.form}>
+          <TextField
+            id="name"
+            onChange={(event) => inputChangeHandler("name", event.target.value)}
+            value={userInput.name}
+            label="Full name"
+            type="text"
             sx={{
-              borderColor: "#EF5742",
-              color: "#EF5742",
+              width: "400px",
+            }}
+          />
+          <TextField
+            id="email"
+            onChange={(event) =>
+              inputChangeHandler("email", event.target.value)
+            }
+            value={userInput.email}
+            label="Email adress *"
+            type="email"
+            sx={{
+              width: "400px",
+            }}
+          />
+          <TextField
+            id="field"
+            onChange={(event) =>
+              inputChangeHandler("field", event.target.value)
+            }
+            select
+            value={userInput.field}
+            label="Department"
+            sx={{
+              width: "400px",
             }}
           >
-            Reset form
-          </Button>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            sx={{
-              color: "white",
-              background: "#EF5742",
-              width: "260px",
-            }}
-          >
-            Add new vistior
-          </Button>
-        </Stack>
-      </FormControl>
+            <MenuItem key={1} value={"Accounting"}>
+              Accounting
+            </MenuItem>
+            <MenuItem key={2} value={"IT"}>
+              IT
+            </MenuItem>
+            <MenuItem key={3} value={"Management"}>
+              Management
+            </MenuItem>
+            <MenuItem key={4} value={"Marketing"}>
+              Marketing
+            </MenuItem>
+            <MenuItem key={5} value={"Sales"}>
+              Sales
+            </MenuItem>
+          </TextField>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="I agree to be added to the table"
+          />
+          <Stack spacing={2} direction="row">
+            <Button
+              onClick={resetHandler}
+              variant="outlined"
+              color="warning"
+              sx={{
+                borderColor: "#EF5742",
+                color: "#EF5742",
+              }}
+            >
+              Reset form
+            </Button>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{
+                color: "white",
+                background: "#EF5742",
+                width: "260px",
+              }}
+            >
+              Add new vistior
+            </Button>
+          </Stack>
+        </FormControl>
+      </form>
     </div>
   );
 };
