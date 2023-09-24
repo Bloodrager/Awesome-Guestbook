39496115c7cc30ae9d1e22928205524d409f7d9c
rastislav <rabvoc@gmail.com>
2023-09-24 22:00:18 +0200

    Fix Fields collum

--- src/components/VisitorManagement/VisitorManagement.js
+++ src/components/VisitorManagement/VisitorManagement.js
@@ -24,7 +24,7 @@ const VisitorManagement = (props) => {
       align: "left",
     },
     {
-      field: "select",
+      field: "field",
       headerName: "Department",
       width: 330,
       sortable: false,
